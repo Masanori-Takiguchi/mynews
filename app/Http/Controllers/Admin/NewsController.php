b8ba6e945178b958c35acc24ea4e97f3d125deaf
Masanori Takiguchi <masa.ballet.1116@icloud.com>
2020-11-05 03:13:15 +0000

    heroku

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\History;
+use App\News;
 use Carbon\Carbon;
 class NewsController extends Controller
 {
